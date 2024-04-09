e31011d2764e89ba1de105d9e0a9a33cd375b454
Alejandro Pérez Viera <jamon9990@gmail.com>
2024-04-09 11:36:53 +0100

    Commit estructura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/streaming', pathMatch: 'full' },
+  { path: 'streaming', component: LandingComponent },
+];
