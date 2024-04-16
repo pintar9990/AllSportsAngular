c71f1594e082ae6ac0984bbbdb1cf9c5d7422ddb
Alejandro Pérez Viera <jamon9990@gmail.com>
2024-04-16 11:44:06 +0100

    Edit Profile Component added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import {LandingComponent} from "./pages/landing/landing.component";
 import {LoginFormularyComponent} from "./pages/login-formulary/login-formulary.component";
 import {RegisterFormularyComponent} from "./pages/register-formulary/register-formulary.component";
+import {EditProfileComponent} from "./pages/edit-profile/edit-profile.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
   { path: 'login', component: LoginFormularyComponent },
   { path: 'register', component: RegisterFormularyComponent },
+  { path: 'edit', component: EditProfileComponent}
 
 ];
