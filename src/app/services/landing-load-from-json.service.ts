974b296da7abf79a04a01eac474246038af09860
CarlosMireles <carlosmirelesrodriguez1@gmail.com>
2024-04-19 12:33:10 +0100

    servicio carga trending json

--- src/app/services/landing-load-from-json.service.ts
+++ src/app/services/landing-load-from-json.service.ts
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
-export class LandingLoadFromJsonService {
+export class LandingLoadFromJSONService {
 
   constructor(private http: HttpClient) { }
 
