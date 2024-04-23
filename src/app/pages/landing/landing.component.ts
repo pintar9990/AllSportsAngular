0915f68127d84b057e7035eac5dc089db479eaf8
CarlosMireles <carlosmirelesrodriguez1@gmail.com>
2024-04-23 11:46:13 +0100

    landing

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {League} from "../../models/leagues/league";
+import {Team} from "../../models/teams/team";
+import {LandingLoadFromJSONService} from "../../services/landing-load-from-json.service";
+import {NgForOf, NgIf} from "@angular/common";
+import {LeagueArtDisplayComponent} from "../../components/league-art-display/league-art-display.component";
+import {TeamLogoDisplayComponent} from "../../components/team-logo-display/team-logo-display.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    LeagueArtDisplayComponent,
+    TeamLogoDisplayComponent
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit{
+    leagues?: League[];
+    teams?: Team[];
+
+    constructor(private dataService: LandingLoadFromJSONService) {}
+    ngOnInit(): void {
+        this.dataService.getLeagues().subscribe(data => {
+          this.leagues = data;
+        });
+        this.dataService.getTeams().subscribe(data => {
+          this.teams = data;
+        });
+    }
 
 }
--- src/app/services/landing-load-from-json.service.ts
+++ src/app/services/landing-load-from-json.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {League} from "../models/leagues/league";
 import {Team} from "../models/teams/team";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 
 
 @Injectable({
@@ -13,10 +13,14 @@ export class LandingLoadFromJSONService {
   constructor(private http: HttpClient) { }
 
   getLeagues(): Observable<League[]> {
-    return this.http.get<League[]>('assets/trending.json');
+    return this.http.get<{ leagues: League[] }>('assets/trending.json').pipe(
+      map(response => response.leagues)
+    );
   }
 
   getTeams(): Observable<Team[]> {
-    return this.http.get<Team[]>('assets/trending.json');
+    return this.http.get<{ teams: Team[] }>('assets/trending.json').pipe(
+      map(response => response.teams)
+    );
   }
 }
